da5cb2dc6d6e75e23d94d3bd2a8d9c64ee6030e8
Anselmo Alves Junior <anselmo.junior@eldorado.org.br>
2019-12-26 12:40:11 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -11,8 +11,8 @@ public class Reservation {
     
     private static SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer rootNumber, Date checkIn, Date checkOut) {
-        this.roomNumber = rootNumber;
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
@@ -38,9 +38,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
     }
     
-    public void updateDates(Date checkIn,Date checkOut){
+    public String updateDates(Date checkIn,Date checkOut){
+        Date now=new Date();
+        
+        if(checkIn.before(now) || checkOut.before(now)){
+            return "Reservation dates for update must be future dates";
+        }
+        if(!checkOut.after(checkIn)){
+            return "Check-out date must be after check-in date";
+        }
         this.checkIn=checkIn;
         this.checkOut=checkOut;
+        return null;
     }
     
     @Override
